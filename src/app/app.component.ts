716720c8c165c874f0f6e201f4aea1a5d47dfaf3
SASHANK DEB <sashankdebrgu7@gmail.com>
2021-08-11 22:30:25 +0530

    chg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
-export class AppComponent {
-  title = 'Ecommerce';
-  clickMessage = 'Are Deewano Mujeh Pehechano';
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+  })
+  export class AppComponent {
+    title = 'Ecommerce';
+    clickMessage = 'Are Deewano Mujeh Pehechano';
 
-  onClickMe() {
-    this.clickMessage = 'Don Don Don Don Don';
-}
+    onClickMe() {
+      this.clickMessage = 'Don Don Don Don Don';
+  }
 }
